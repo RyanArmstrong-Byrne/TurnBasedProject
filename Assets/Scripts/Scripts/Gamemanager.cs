d0d948cd945c36c95d79f8ad7ddb6c79e1201b5e
RyanArmstrong-Byrne <ryanab@westnet.com.au>
2025-02-20 14:02:07 +0800

    added new Menu UI

---
+++ Assets/Scripts/Scripts/Gamemanager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public GameStates state = GameStates.PlayerTurn;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null && instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum GameStates
+{
+    PlayerTurn,EnemyTurn, Pause, Menu, Win, Lose
+}
